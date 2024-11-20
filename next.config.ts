1b27a952a43aa7fe9151f1940a5afe54b98e9a29
Jame Zhan <jame@orderly.network>
2024-11-20 17:47:03 +0800

    fix can't resolve 'pino-pretty' warning

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  webpack: (config) => {
+    // fix Module not found: Can't resolve 'pino-pretty' warning https://github.com/pinojs/pino/issues/688
+    config.externals = [...config.externals, "pino-pretty"];
+    return config;
+  },
   reactStrictMode: false,
   async redirects() {
     return [
